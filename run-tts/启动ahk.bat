b59d484976adff2056dc7a6de7fe52abeef1b886
xymsyh <xymsyh@gmail.com>
2025-11-15 14:49:09 +0800

    更新启动脚本，增加Epic Pen启动提示，优化成功启动信息

--- run-tts/启动ahk.bat
+++ run-tts/启动ahk.bat
@@ -1,5 +1,8 @@
 @echo off
-echo Starting AHK scripts...
+echo Starting AHK scripts and Epic Pen...
+
+:: Launch Epic Pen
+start "" "D:\Program Files (x86)\Epic Pen\epicpen.exe"
 
 :: Launch first AHK script
 start "" "D:\R2025\AHK\ahk-script\run-tts\run-tts-3.ahk"
@@ -7,5 +10,5 @@ start "" "D:\R2025\AHK\ahk-script\run-tts\run-tts-3.ahk"
 :: Launch second AHK script
 start "" "D:\R2025\AHK\ahk-script\run-tts\run-tts-4.ahk"
 
-echo Scripts started successfully!
+echo All scripts and programs started successfully!
 pause
